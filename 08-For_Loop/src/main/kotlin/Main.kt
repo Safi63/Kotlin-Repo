aee008a3da69adefd414564265410c0e3603486a
Safi Ullah Muhammad <96912261+Safi63@users.noreply.github.com>
2022-04-06 23:50:19 +0500

    For Loop

---
+++ 08-For_Loop/src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+
+//        for(i in 1..5){
+//            println(i)
+//        }
+//        println(" ")
+//
+//    for (i in 1..5 step 2){
+//            println(i)
+//        }
+//         println(" ")
+//
+//    for (i in 1 until 5){
+//            println(i)
+//        }
+//         println(" ")
+//
+//    for (i in 10 downTo 1 step 2){
+//            println(i)
+//        }
+
+    println("Printing table of 2")
+    var num = 2
+    for (i in 1..10)
+        println("$num x $i = ${num*i} ")
+    }
+}
