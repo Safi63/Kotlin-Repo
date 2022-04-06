a673e5d6cf095f76afe4208644d80e39c3b2a322
Safi Ullah Muhammad <96912261+Safi63@users.noreply.github.com>
2022-04-06 12:22:36 +0500

    My first commit

---
+++ 01-Hello_World_Program/HelloWorld/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("Hello World!...This is my First Kotlin Program")
+    println("below I will perform some simple mathematical functions")
+    print(2+2)
+    print(2*2)
+    print(2/2)
+    println("The End")
+}
