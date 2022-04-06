596e200a4475ac70d849d325161e1052b1529205
Safi Ullah Muhammad <96912261+Safi63@users.noreply.github.com>
2022-04-06 15:41:14 +0500

    thirdFinal

--- 03-Arithmetic_Relational_Operators/src/main/kotlin/Main.kt
+++ 03-Arithmetic_Relational_Operators/src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 fun main(args: Array<String>) {
-    println("Arithmetic and Relational Operators" +
-            "")
+    println("Arithmetic and Relational Operators ... Check these files" +
+            "ArithmeticOperators.kt" +
+            "RelationlOperators.kt" +
+            "PrePost_Increment_Decrement.kt")
 }
